add827e94a7b2e590a3270f1934bd703555e49c7
guan <iloveweekdays@outlook.com>
2021-08-18 01:04:36 +0800

    gradle and maven both can run boot in the command line

--- src/main/java/process/citing/files/DemoApplication.java
+++ src/main/java/process/citing/files/DemoApplication.java
@@ -3,6 +3,8 @@ package process.citing.files;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,7 +14,8 @@ import java.util.Arrays;
 
 @ServletComponentScan
 @SpringBootApplication
-public class DemoApplication {
+@ComponentScan({"process.citing.files","process.citing.files.shared"})
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
